87cf38234eba9c78398413061f144e3f43dfc341
Artem Eremeev <jeremy1002@yandex.ru>
2023-07-30 16:18:49 +0300

    chore: Initial commit

---
+++ GoogleMapsSwiftUI/GoogleMapsSwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  GoogleMapsSwiftUI.h
+//  GoogleMapsSwiftUI
+//
+//  Created by Artem Eremeev on 30.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleMapsSwiftUI.
+FOUNDATION_EXPORT double GoogleMapsSwiftUIVersionNumber;
+
+//! Project version string for GoogleMapsSwiftUI.
+FOUNDATION_EXPORT const unsigned char GoogleMapsSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapsSwiftUI/PublicHeader.h>
+
+
